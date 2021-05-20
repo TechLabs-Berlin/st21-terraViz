539fcc8a5126c8d739f61e25c055f25da74efc09
seongeunkimmy <kimusique7@gmail.com>
2021-05-20 13:25:53 +0200

    create-react-app for setup

---
+++ terraviz/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Hello from './app.js';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Hello />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+
