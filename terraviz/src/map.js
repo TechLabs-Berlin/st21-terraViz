6b4bbe1a1e21d7e48d98b2b429e6faa90826b81e
Timea Varju <miavarju011@gmail.com>
2021-05-26 20:56:01 +0200

    initial setup for the base map

--- terraviz/src/map.js
+++ terraviz/src/map.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
+import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+// Insert your access token here
+mapboxgl.accessToken = "";
 
+// Initialize base map with basic settings
 const Map = () => {
-  return <div>Base map</div>;
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  useEffect(() => {
+    if (map.current) return;
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/light-v10",
+      center: [17, 30],
+      zoom: 1,
+    });
+  });
+  return (
+    <div>
+      <div ref={mapContainer} className="map-container" />
+    </div>
+  );
 };
 
 export default Map;
