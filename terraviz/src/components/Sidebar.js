bddb27e3f5a82153c3b85a606c615d9d8969c7c2
seongeunkimmy <kimusique7@gmail.com>
2021-05-28 19:40:06 +0200

    Sidebar css basic structure

--- terraviz/src/components/Sidebar.js
+++ terraviz/src/components/Sidebar.js
@@ -11,14 +11,27 @@ function Sidebar() {
         <>
         <IoIosArrowDropright className={sidebar ? 'nav-open' : 'hidden'} onClick={toggleSidebar} />
         <div className="Sidebar">
+        
          <nav className = {sidebar ? 'nav active' : 'nav'}>
+           
              <ul className = 'nav-items'>
+            
                  <li className='nav-toggle' >
                   <IoIosArrowDropleft onClick={toggleSidebar} />
                  </li>
-                 <li className='nav-list'>Legend</li>
-                 <li className='nav-list'>Chart</li>
-                 <li className='nav-list'>Summary</li>
+                 <h1>Terraviz</h1>
+                 <div className='nav-list'>
+                 <li>Legend</li>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                 </div>
+                 <div className='nav-list'>
+                  <li>Chart</li>
+                 </div>
+                 <div className='nav-list'>
+                 <li>Summary</li>
+                 <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                 </div>
+                
              </ul>
          </nav>
          </div>
