8462d855d7da23ca4f478c28d613aea32136fa43
Timea Varju <miavarju011@gmail.com>
2021-06-06 20:13:54 +0200

    add UI for country/city switch

--- terraviz/src/app.js
+++ terraviz/src/app.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Map from "./components/Map.js";
+import CountrySwitch from "./components/CountrySwitch.js";
+import "./components/CountrySwitch.css";
 import "./app.css";
 
 const App = () => {
   return (
     <div>
+      <CountrySwitch />
       <Map />
     </div>
   );
