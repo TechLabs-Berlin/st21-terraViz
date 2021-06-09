b331aea58bd4e7dd675e93d852bade24fe0e3af7
Timea Varju <miavarju011@gmail.com>
2021-06-09 18:29:48 +0200

    time slider shows current year interactively

--- terraviz/src/components/TimeSlider.js
+++ terraviz/src/components/TimeSlider.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoIosPlay } from "react-icons/io";
 import "./TimeSlider.css";
 
 const TimeSlider = () => {
-  // return <div className="slider-container"></div>;
+  const [currentYear, setCurrentYear] = useState(1750);
+
   return (
     <div className="slider-main-container">
       <div className="slider-wrapper">
-        <input type="range" min="1750" max="2013" className="slider-range" />
+        <input
+          type="range"
+          min="1750"
+          max="2013"
+          className="slider-range"
+          onChange={(e) => setCurrentYear(e.target.value)}
+        />
         <div className="label-wrapper">
           <div className="label-left">1750</div>
           <div className="label-right">2013</div>
@@ -15,7 +22,7 @@ const TimeSlider = () => {
       </div>
 
       <IoIosPlay className="play-button" />
-      <span className="current-date">1920</span>
+      <span className="current-date">{currentYear}</span>
     </div>
   );
 };
