5c8dcae2a3af5ebcafe0f64a00acba7365096684
Timea Varju <miavarju011@gmail.com>
2021-06-22 14:14:15 +0200

    restructure tags and class names

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,43 +8,56 @@ function Sidebar() {
   return (
     <>
       <IoIosArrowDropright
-        className={sidebar ? "nav-open" : "hidden"}
+        className={
+          sidebar ? "outer-toggle-icon-visible" : "outer-toggle-icon-hidden"
+        }
         onClick={onToggleSidebar}
       />
-      <div className="Sidebar">
-        <nav className={sidebar ? "nav active" : "nav"}>
-          <ul className="nav-items">
-          <div className="title">
-          <h1>TerraViz</h1>
-            <li className="nav-toggle">
-              <IoIosArrowDropleft onClick={onToggleSidebar} />
-            </li>
-            </div>
-           
-            <div className="nav-list">
-              <li>The problem</li>
-              <span>Where is our project grounded?</span>
-              <p>
-              Climate change is THE largest problem of humanity.
-              Making the future of our planet tangible is one of the psychological barriers that have made climate change into an elusive problem.  
-              </p>
-            </div>
-            <div className="nav-list">
-              <li>Our product</li>
-              <span>What we built</span>
-              <p>
-              We visualize the impact climate change has on our planet with a web-application that makes planet data easily accessible and visually attractive for users.
-              </p>
-            </div>
-            <div className="nav-list">
-              <li>How To</li>
-              <span>How the app works</span>
-              <p>
-              We visualize the impact climate change has on our planet with a web-application that makes planet data easily accessible and visually attractive for users.
-              </p>
-            </div>
-          </ul>
-        </nav>
+
+      <div className={sidebar ? "sidebar-visible hidden" : "sidebar-visible"}>
+        <IoIosArrowDropleft
+          className="inner-toggle-icon"
+          onClick={onToggleSidebar}
+        />
+        <h1>TerraViz</h1>
+
+        <section className="">
+          <h2>The problem</h2>
+          <span>Where is our project grounded?</span>
+          <p>
+            Climate change is THE largest problem of humanity. Making the future
+            of our planet tangible is one of the psychological barriers that
+            have made climate change into an elusive problem.
+          </p>
+        </section>
+        <section className="">
+          <h2>Our product</h2>
+          <span>What we built</span>
+          <p>
+            We visualize the impact climate change has on our planet with a
+            web-application that makes planet data easily accessible and
+            visually attractive for users.
+          </p>
+        </section>
+        <section className="">
+          <h2>How To</h2>
+          <span>How the app works</span>
+          <p>
+            We visualize the impact climate change has on our planet with a
+            web-application that makes planet data easily accessible and
+            visually attractive for users.
+          </p>
+          <p>
+            We visualize the impact climate change has on our planet with a
+            web-application that makes planet data easily accessible and
+            visually attractive for users.
+          </p>
+          <p>
+            We visualize the impact climate change has on our planet with a
+            web-application that makes planet data easily accessible and
+            visually attractive for users.
+          </p>
+        </section>
       </div>
     </>
   );
