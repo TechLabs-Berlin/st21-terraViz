cfbe05d735a49fce1d8d91bbd01a1fdad6968dde
Timea Varju <miavarju011@gmail.com>
2021-06-29 23:40:42 +0200

    add color&radius schema,connect data to TimeSlider

--- frontend/src/components/Map.js
+++ frontend/src/components/Map.js
@@ -2,10 +2,40 @@ import React, { useState, useRef, useEffect } from "react";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 import "./Map.css";
 
+// Constants
 mapboxgl.accessToken =
   "pk.eyJ1IjoidGVycmF2aXoiLCJhIjoiY2twbW9ldG5vMGsyeDJ3cXF2eTBtNmozaSJ9.cuNhWme-R-RK02H3qWxHWw";
 
-const Map = () => {
+const COLOR_TEMP = {
+  negativeBig: 3,
+  negative: 7,
+  base: 10,
+  positive: 11,
+  positiveBig: 15,
+};
+
+const COLOR_HEX = {
+  negativeBig: "#7BBAD8",
+  negative: "#A1D4EC",
+  base: "#F5E9C8",
+  positive: "#F29488",
+  positiveBig: "#E86A62",
+};
+
+const RADIUS_TEMP = {
+  low: 3,
+  base: 10,
+  high: 15,
+};
+
+const RADIUS_SIZE = {
+  base: 5,
+  change: 25,
+};
+
+// Main component
+const Map = (props) => {
+  const { currentYear } = props;
   const mapContainer = useRef(null);
   const [map, setMap] = useState(null);
 
@@ -32,8 +62,9 @@ const Map = () => {
             type: "geojson",
             data: data,
           });
+
           defaultMap.addLayer({
-            id: "circles",
+            id: "country-circles",
             source: "point",
             type: "circle",
             paint: {
@@ -41,28 +72,56 @@ const Map = () => {
                 "interpolate",
                 ["linear"],
                 ["get", "temp"],
-                3,
-                "#7BBAD8",
-                7,
-                "#A1D4EC",
-                9,
-                "#F5E9C8",
-                11,
-                "#F29488",
-                15,
-                "#E86A62",
+                COLOR_TEMP.negativeBig,
+                COLOR_HEX.negativeBig,
+                COLOR_TEMP.negative,
+                COLOR_HEX.negative,
+                COLOR_TEMP.base,
+                COLOR_HEX.base,
+                COLOR_TEMP.positive,
+                COLOR_HEX.positive,
+                COLOR_TEMP.positiveBig,
+                COLOR_HEX.positiveBig,
               ],
               "circle-opacity": 0.8,
-              "circle-radius": 17,
+              "circle-radius": [
+                "interpolate",
+                ["linear"],
+                ["get", "temp"],
+                RADIUS_TEMP.low,
+                RADIUS_SIZE.change,
+                RADIUS_TEMP.base,
+                RADIUS_SIZE.base,
+                RADIUS_TEMP.high,
+                RADIUS_SIZE.change,
+              ],
             },
             layout: {},
           });
-        })
-        .catch((err) => console.error(err));
-    });
 
-    setMap(defaultMap);
+          // Filter the data points and show only those where the "year" property matches
+          // with the currentYear of the TimeSlider component.
+          defaultMap.setFilter("country-circles", [
+            "==",
+            ["get", "year"],
+            currentYear,
+          ]);
+        })
+
+        .catch((err) => console.error(err));
+
+      setMap(defaultMap);
+    });
   }, [map]);
+
+  // This useEffect() takes care of the data filtering after the initial load has happened
+  useEffect(() => {
+    if (map === null) {
+      return;
+    }
+
+    map.setFilter("country-circles", ["==", ["get", "year"], currentYear]);
+  }, [currentYear, map]);
 
   return (
     <div>
