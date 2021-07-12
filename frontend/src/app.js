409e8e7e7b3f4bcf0540f083be40fc0ea50a5834
Timea Varju <miavarju011@gmail.com>
2021-07-12 19:17:37 +0200

    remove import statement

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import Sidebar from "./components/Sidebar.js";
-// import CountrySwitch from "./components/CountrySwitch.js";
 import TimeSlider from "./components/TimeSlider.js";
 import Map from "./components/Map.js";
 import main_logo from "./assets/TerraViz Logo White.png";
-import "./components/CountrySwitch.css";
 import "./app.css";
 
 const App = () => {
@@ -13,7 +11,6 @@ const App = () => {
   return (
     <div>
       <Sidebar />
-      {/* <CountrySwitch /> */}
       <img className="main_logo" src={main_logo} alt="main-logo" />
       <TimeSlider currentYear={currentYear} setCurrentYear={setCurrentYear} />
       <Map currentYear={currentYear} />
